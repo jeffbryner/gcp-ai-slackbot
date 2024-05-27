b3fed16c699b8a69c27c82868838e7b755b26f2f
Jeff Bryner <jeff@jeffbryner.com>
2024-05-27 15:43:36 -0700

    switch to gemini, add summary

--- code/source/main.py
+++ code/source/main.py
@@ -3,16 +3,20 @@ import base64
 import os
 import json
 from slack_bolt import App
+from slack_sdk.errors import SlackApiError
+
 from flask import Flask, request
-from utils import get_secret, getValueByPath
+from utils import get_secret, get_channel_messages, get_message_thread
 from google.cloud import pubsub_v1
 import vertexai
-from vertexai.language_models import (
-    TextGenerationModel,
-    TextEmbeddingModel,
-    ChatModel,
-    InputOutputTextPair,
-)
+from datetime import datetime, date, timezone, timedelta
+import time
+from operator import itemgetter
+from fastnumbers import try_float
+
+
+# from vertexai.language_models import TextGenerationModel
+from vertexai.generative_models import GenerativeModel
 
 
 # Flask adapter
@@ -20,18 +24,14 @@ from slack_bolt.adapter.flask import SlackRequestHandler
 
 
 # TODOs
-# - Add AI
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 PROJECT_ID = os.environ.get("PROJECT_ID", "")
 # just the ID not the projects/project-id replace if exists
 PROJECT_ID = PROJECT_ID.replace("projects/", "")
 
-# init vertexAI
-vertexai.init(project=PROJECT_ID, location="us-central1")
-generation_model = TextGenerationModel.from_pretrained("text-bison@001")
 
 # initialize slack
 # process_before_response must be True when running on cloud functions, must be false for cloud run
@@ -47,7 +47,12 @@ slack_app = App(
     ),
 )
 
-slack_client = None
+slack_client = slack_app.client
+
+
+def slack_markdown(text):
+    text = text.replace("**", "*")
+    return text
 
 
 def send_pubsub_message(message):
@@ -57,7 +62,7 @@ def send_pubsub_message(message):
         topic_path = f"projects/{PROJECT_ID}/topics/slack-messages"
         # the message we send
         message_body = json.dumps(message).encode("utf-8")
-        logger.info(
+        logger.debug(
             f"message is {type(message)} and message_body is {type(message_body)}"
         )
         _ = publisher.publish(topic_path, message_body)
@@ -68,28 +73,216 @@ def send_pubsub_message(message):
 def handle_slack_message(message):
     logger.debug(f"handle_slack_message received: {message}")
     try:
+        # init vertexAI
+        vertexai.init(project=PROJECT_ID, location="us-central1")
+        # generation_model = TextGenerationModel.from_pretrained("text-bison")
+        generation_model = GenerativeModel("gemini-1.5-pro")
+        generation_config = {
+            "temperature": 1,
+            "max_output_tokens": 2048,
+            "top_p": 1.0,
+            "top_k": 20,
+        }
+
         # if we haven't already started a thread, start one
         if message["entrypoint"] == "greetings" and "thread_ts" not in message:
             # get a welcome from AI
 
-            prompt = "Generate a friendly welcome message"
-            vertext_response = generation_model.predict(prompt=prompt)
+            prompt = "You are a slack bot. The user has summoned you into a slack thread. Generate a friendly welcome message"
+            vertext_response = generation_model.generate_content(
+                contents=prompt, generation_config=generation_config
+            )
+            logger.debug(f"vertext response: {vertext_response}")
 
             slack_result = slack_client.chat_postMessage(
                 channel=message["channel"],
                 text=f".. <@{message['user']}> {vertext_response.text}",
+                mrkdwn=True,
                 thread_ts=message["ts"],
             )
             logger.debug(slack_result)
+        # is it an existing thread
         if message["entrypoint"] == "thread_reply" and "thread_ts" in message:
-            prompt = message["text"]
-            vertext_response = generation_model.predict(prompt=prompt)
-            slack_result = slack_client.chat_postMessage(
+            # check if we are in the thread so we don't reply to random threads
+            reply_with_ai = False
+            slack_identity = slack_client.auth_test()
+            SLACK_USER_ID = slack_identity["user_id"]
+            # see if this thread involves us
+            # search the history based on this thread timestamp
+            slack_result = slack_client.conversations_history(
                 channel=message["channel"],
-                text=vertext_response.text,
-                thread_ts=message["ts"],
+                oldest=message["thread_ts"],
+                inclusive=True,
+                limit=1,
             )
+            logger.debug("HERE IS THE THREAD LEAD IN")
             logger.debug(slack_result)
+
+            if (
+                slack_result
+                and "messages" in slack_result
+                and SLACK_USER_ID in slack_result["messages"][0]["reply_users"]
+            ):
+                logger.debug("WE ARE IN THE THREAD")
+                reply_with_ai = True
+            else:
+                logger.debug("THIS ISN'T A THREAD FOR US")
+            if reply_with_ai:
+                prompt = f"You are a slack bot in a thread with a person. Please respond kindly to this prompt using slack markdown formatting: \n {message['text']}"
+                # vertext_response = generation_model.predict(
+                #     prompt=prompt, temperature=1, max_output_tokens=2048
+                # )
+                vertext_response = generation_model.generate_content(
+                    contents=prompt, generation_config=generation_config
+                )
+                logger.debug(f"vertext response: {vertext_response}")
+                ai_response = vertext_response.text
+
+                if not ai_response:
+                    ai_response = "Hrm.. dunno how to respond"
+
+                blocks = [
+                    {
+                        "type": "section",
+                        "text": {"type": "mrkdwn", "text": slack_markdown(ai_response)},
+                    }
+                ]
+                slack_result = slack_client.chat_postMessage(
+                    channel=message["channel"],
+                    blocks=blocks,
+                    thread_ts=message["ts"],
+                )
+                logger.debug(slack_result)
+
+        if message["entrypoint"] == "summarize_request":
+            # we've been asked to summarize a channel
+            try:
+                slack_result = slack_client.conversations_info(
+                    channel=message["channel_id"]
+                )
+                # are we in the channel?
+                if not slack_result.data["channel"]["is_member"]:
+                    slack_client.conversations_join(channel=message["channel_id"])
+            except Exception as e:
+                logger.error("Error accessing channel: {}".format(e))
+                # start a DM with the user to tell them we aren't in the channel
+                slack_result = slack_client.conversations_open(users=message["user_id"])
+                slack_client.chat_postMessage(
+                    channel=slack_result["channel"]["id"],
+                    text="I don't seem to have access to the channel to summarize for you, sorry!",
+                )
+                return
+            # what's the latest in the channel?
+            slack_result = slack_client.conversations_history(
+                limit=1, channel=message["channel_id"]
+            )
+            # anything to summarize?
+            if "messages" not in slack_result.data or not len(
+                slack_result.data["messages"]
+            ):
+                slack_client.chat_postEphemeral(
+                    channel=message["channel_id"],
+                    user=message["user_id"],
+                    text="Doesn't appear to be any messages to summarize",
+                )
+                return
+            # get the latest timestamp
+            latest_timestamp = slack_result.data["messages"][0]["ts"]
+            # set the lookback period
+            lookback = 1
+            try:
+                prompt = f"""
+                A person asked you to summarize content for a slack channel. The person invoking you said: 
+
+                'summarize {message['text']} '
+
+                If the person gave you a time frame, what number of hours? 
+                If the person did not give a time frame assume 1 hour. 
+                Answer with only the number.
+                """
+
+                response = generation_model.generate_content(
+                    contents=prompt,
+                    generation_config={
+                        "temperature": 0.1,
+                        "max_output_tokens": 8,
+                        "top_p": 1.0,
+                        "top_k": 40,
+                    },
+                )
+                lookback = try_float(response.text, nan=1, on_fail=1)
+
+            except Exception as e:
+                logger.error(f"Error determining lookback period: {e}")
+
+            search_timestamp = datetime.fromtimestamp(
+                float(latest_timestamp)
+            ) - timedelta(hours=lookback)
+            # convert to a timestamp
+            search_timestamp = time.mktime(search_timestamp.timetuple())
+            # search the channel
+            # get full channel history with all threads
+            channel_id = message["channel_id"]
+            channel_history = get_channel_messages(
+                slack_client, channel_id, oldest=search_timestamp
+            )
+            thread_history = []
+            for history_message in channel_history:
+                if "thread_ts" in history_message:
+                    thread_messages = get_message_thread(
+                        slack_client,
+                        channel_id=channel_id,
+                        thread_ts=history_message["thread_ts"],
+                    )
+                    thread_history.extend(thread_messages)
+            # add the threads
+            channel_history.extend(thread_history)
+            # sort it all by ts
+            sorted_history = sorted(channel_history, key=itemgetter("ts"))
+            # add any threads
+            channel_conversation = ""
+            # cache of user id to name
+            slack_users = {}
+            for slack_message in sorted_history:
+                # resolve user ids if we don't already know them
+                slack_user_id = slack_message["user"]
+                user_name = ""
+                if slack_user_id not in slack_users:
+                    user_result = slack_client.users_info(user=slack_message["user"])
+                    user_name = user_result["user"]["profile"]["real_name"]
+                    slack_users[slack_user_id] = user_name
+                else:
+                    user_name = slack_users[slack_user_id]
+                channel_conversation += f"{user_name}: {slack_message['text']}\n"
+
+            # Prompt the AI
+            prompt = f"You are a slackbot and have been asked to summarize the following conversation:\n {channel_conversation}"
+            vertext_response = generation_model.generate_content(
+                contents=prompt, generation_config=generation_config
+            )
+            logger.debug(f"vertext response: {vertext_response}")
+            ai_response = vertext_response.text
+            if not ai_response:
+                ai_response = "Hrm.. dunno how to respond"
+            lookback_timeperiod = "hour"
+            if lookback > 1:
+                lookback_timeperiod = "hours"
+
+            blocks = [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": slack_markdown(
+                            f"Summary of the last {lookback} {lookback_timeperiod}:\n {ai_response}"
+                        ),
+                    },
+                }
+            ]
+            slack_result = slack_client.chat_postEphemeral(
+                channel=message["channel_id"], user=message["user_id"], blocks=blocks
+            )
+
     except Exception as e:
         logger.error(f"Error posting message: {e}")
 
@@ -99,29 +292,36 @@ def ack_message(ack):
     ack(f"on it")
 
 
-@slack_app.message("hello ai|howdy ai|<!here>|hey ai")
-def greetings(ack, client, say, message):
-    global slack_client
-    slack_client = client
-    ack()
+def greetings(message):
     message["entrypoint"] = "greetings"
     logger.debug(message)
     send_pubsub_message(message)
 
 
-@slack_app.event("message")
-def thread_reply(ack, client, message):
+slack_app.message("hello ai|howdy ai|<!here>|hey ai")(ack=ack_message, lazy=[greetings])
+
+
+def thread_reply(message):
     # should be subtype of message_replied, but a bug in events api omits it
     # so we check for thread_ts
     logger.debug(f"app.event message received: {message}")
-    ack()
     if "parent_user_id" in message and "thread_ts" in message:
-        global slack_client
-        slack_client = client
         message["entrypoint"] = "thread_reply"
         logger.debug(message)
         send_pubsub_message(message)
 
+
+slack_app.event("message")(ack=ack_message, lazy=[thread_reply])
+
+
+def summarize(ack, command, respond):
+    logger.debug(command)
+    command["entrypoint"] = "summarize_request"
+    send_pubsub_message(command)
+    respond("")
+
+
+slack_app.command("/summarize")(ack=ack_message, lazy=[summarize])
 
 flask_app = Flask(__name__)
 
@@ -154,7 +354,7 @@ def default_post_route():
     headers = dict(request.headers)
     headers.pop("Authorization", None)  # avoid logging secrets
     logger.debug(f"headers: {headers}")
-    logger.info(f"{content_type} request received")
+    logger.debug(f"{content_type} request received")
 
     if content_type == "application/json":
         envelope = request.json
